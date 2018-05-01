d82fad601d54576c24ce69b2ca2ca969d4895a0d
DESKTOP-M9IB1VS\_miu_minu <minunu93@naver.com>
2018-05-02 06:00:09 +0900

    탭에 추가 오류 수정중

--- app/src/main/java/cse/underdog/org/underdog_client/memo/MemoFragment.java
+++ app/src/main/java/cse/underdog/org/underdog_client/memo/MemoFragment.java
@@ -1,5 +1,8 @@
 package cse.underdog.org.underdog_client.memo;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,9 +10,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import cse.underdog.org.underdog_client.MainActivity;
 import cse.underdog.org.underdog_client.R;
 
 public class MemoFragment extends Fragment {
+    private Activity activity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if(context instanceof Activity){
+            activity = (Activity) context;
+        }
+
+        Intent intent = new Intent(activity, MainActivity.class);
+        activity.startActivity(intent);
+
+        activity.finish();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +55,17 @@ public class MemoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_memo, null);
 
+
         return view;
+    }
+
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        if (isVisibleToUser) {
+            // 보인다.
+        } else {
+            // 안보인다.
+        }
+        super.setUserVisibleHint(isVisibleToUser);
     }
 
 }
